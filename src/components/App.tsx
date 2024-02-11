6be88320f3be09ac3c2d113c15c9023cbdbdf174
skywkie <dlunusin@gmail.com>
2024-02-11 21:28:44 +0300

    start

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import Categories from "./Categories/Categories";
+import Header from "./Header/Header";
+
+function App() {
+   return (
+      <div className="wrapper">
+         <Header />
+         <Categories />
+      </div>
+   );
+}
+
+export default App;
